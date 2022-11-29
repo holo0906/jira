848d048f915ecc90adc261c7454735993bb9db33
Sivan <1758299424@qq.com>
2022-11-29 13:42:33 +0800

    feat: 添加登录注册

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { LoginScreen } from "screens/login";
+import { useAuth } from "context/auth-context";
+import { AuthenticatedApp } from "authenticated-app";
+import { UnauthenticatedApp } from "unauthenticated-app";
 import "./App.css";
-import { ProjectListScreen } from "./screens/project-list";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <ProjectListScreen /> */}
-      <LoginScreen />
-    </div>
-  );
+  const { user } = useAuth();
+  return <div className="App">{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
 }
 
 export default App;
