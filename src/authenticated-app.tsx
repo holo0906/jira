a431f0262cc62c2fb6cacfd0cebf4267275629af
Sivan <1758299424@qq.com>
2023-01-11 18:32:40 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,50 @@
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
+
+/**
+ * grid 和 flex 各自的应用场景
+ * 1. 要考虑，是一维布局 还是 二维布局
+ * 一般来说，一维布局用flex，二维布局用grid
+ * 2. 是从内容出发还是从布局出发？
+ * 从内容出发：先有一组内容（数量一般不固定），然后希望它们均匀的分布在容器中，由内容自己的大小决定占据的空间
+ * 从布局出发：先规划网格（数量一般比较固定），然后再把元素往里填充
+ * 从内容出发：flex
+ * 从布局出发：grid
+ */
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main``;
